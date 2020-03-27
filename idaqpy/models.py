968dc108309316771998e51acabf2b21c360b2f9
S. Co1 <sco1.git@gmail.com>
2020-03-27 16:36:34 -0400

    Add log file classification & test

--- idaqpy/models.py
+++ idaqpy/models.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from datetime import datetime
 from enum import Enum, auto
+from fnmatch import fnmatch
 from pathlib import Path
 
 import pandas as pd
@@ -22,10 +23,11 @@ class iDAQ:  # noqa: N801
 
     # Bin supported file extensions to determine parsing behavior
     # NOTE: Downstream processing is case-insensitive
+    # NOTE: Raw log files matching the "LOG.*" pattern are assumed to follow LOG.001, LOG.002, etc.
     log_name_patterns = {
         LogFileType.RAW: ("LOG.*", "*.iDAQ"),  # Will be passed to the log decoder executable
         LogFileType.DECODED: ("*.csv",),
-        LogFileType.MATLAB: ("*.mat"),
+        LogFileType.MATLAB: ("*.mat",),
     }
 
     def __init__(self, data_filepath: Path):
@@ -35,9 +37,28 @@ class iDAQ:  # noqa: N801
         self.raw_data = self.parse_log_file(self.data_filepath)
 
     @classmethod
-    def classify_log_type(cls, file_name: str) -> LogFileType:
-        """Determine type of log file from its filename, as binned by `iDAQ.log_name_patterns`."""
-        raise NotImplementedError
+    def classify_log_type(cls, filepath: Path) -> LogFileType:
+        """
+        Determine type of log file from its filename, as binned by `iDAQ.log_name_patterns`.
+
+        NOTE: Comparison is case-insensitive
+        """
+        file_name = filepath.name.lower()
+        for log_type, patterns in cls.log_name_patterns.items():
+            for pattern in patterns:
+                if fnmatch(file_name, pattern.lower()):
+                    if pattern == "LOG.*":
+                        # Because the default iDAQ log name is LOG.nnn, and the default decoder
+                        # behavior is to append ".csv", we need to special case the raw file
+                        # detection to also check for the all-numeric file extension
+                        if not filepath.suffix[1:].isnumeric():
+                            continue
+                        else:
+                            return log_type
+
+                    return log_type
+        else:
+            return LogFileType.UNSUPPORTED
 
     @classmethod
     def parse_log_file(cls, filepath: Path) -> pd.DataFrame:
@@ -48,18 +69,17 @@ class iDAQ:  # noqa: N801
         raw log files are passed to Wamore's logdecoder executable to be converted into a CSV before
         being parsed into a DataFrame.
         """
-        log_file_name = filepath.name
-        log_type = cls.classify_log_type(log_file_name)
-        if isinstance(log_type, LogFileType.UNSUPPORTED):
+        log_type = cls.classify_log_type(filepath)
+        if log_type == LogFileType.UNSUPPORTED:
             raise UnsupportedLogFile(
-                f"'{log_file_name}' does not match a supported file name pattern. "
+                f"'{filepath.name}' does not match a supported file name pattern. "
                 "A list of supported naming patterns is provided in the README."
             )
 
-        if isinstance(log_type, LogFileType.MATLAB):
+        if log_type == LogFileType.MATLAB:
             raise UnsupportedLogFile("Support for MATLAB files is currently not implemented.")
 
-        if isinstance(log_type, LogFileType.RAW):
+        if log_type == LogFileType.RAW:
             # TODO: Check for logdecoder
             # TODO: Pass log file to logdecoder & replace filepath with output file
             raise NotImplementedError
---
+++ testing/test_log_type_classification.py
@@ -0,0 +1,43 @@
+import typing as t
+from pathlib import Path
+
+import pytest
+import pytest_check as check
+from idaqpy.models import LogFileType, iDAQ
+
+
+TEST_CASES = {
+    LogFileType.RAW: (
+        "LOG.001",
+        "log.001",
+        "Log.001",
+        "LOG.1",
+        "FILE.iDAQ",
+        "file.idaq",
+        "fIlE.IdAq",
+    ),
+    LogFileType.DECODED: (
+        "LOG.001.csv",
+        "log.001_proc.csv",
+        "FILE.001.csv",
+        "TESTING.csv",
+        "TESTING.CSV",
+    ),
+    LogFileType.MATLAB: ("LOG.001.mat", "LOG.001_proc.mat", "LOG.001.MAT", "FILE.mat", "file.mat",),
+    LogFileType.UNSUPPORTED: ("LOG.001.JSON", "file.exe", "file.py", "FILE.py",),
+}
+
+
+@pytest.fixture(params=TEST_CASES.keys())
+def build_test_case(request) -> t.Tuple[LogFileType, t.Tuple[str]]:  # noqa: ANN001
+    """Create a fixture for the provided test cases."""
+    log_type = request.param
+    return log_type, TEST_CASES[log_type]
+
+
+def test_log_classification(build_test_case: t.Tuple[LogFileType, t.Tuple[str]]) -> None:
+    """Test for correct classification of log types."""
+    log_type, file_names = build_test_case
+    for file_name in file_names:
+        failure_msg = f"Comparison failed for filename: '{file_name}'"
+        check.equal(log_type, iDAQ.classify_log_type(Path(file_name)), msg=failure_msg)
