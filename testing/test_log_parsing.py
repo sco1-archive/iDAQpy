242c097c64561ccb13f981dafee4458f25b519a6
S. Co1 <sco1.git@gmail.com>
2020-03-27 18:37:53 -0400

    Frame log parsing flow test

---
+++ testing/test_log_parsing.py
@@ -0,0 +1,32 @@
+import typing as t
+from pathlib import Path
+
+import pytest
+from idaqpy import UnsupportedLogFile
+from idaqpy.models import iDAQ
+
+
+class UnsupportedFileTestCase(t.NamedTuple):
+    """Represent unsupported file test cases & whether the filename should yield an error."""
+
+    filepath: Path
+    should_raise: bool
+
+
+UNSUPPORTED_FILE_CASES = (
+    UnsupportedFileTestCase(filepath=Path("file.exe"), should_raise=True),
+    UnsupportedFileTestCase(filepath=Path("file.mat"), should_raise=True),
+    UnsupportedFileTestCase(filepath=Path("LOG.001"), should_raise=False),
+    UnsupportedFileTestCase(filepath=Path("file.idaq"), should_raise=False),
+    UnsupportedFileTestCase(filepath=Path("file.csv"), should_raise=False),
+)
+
+
+@pytest.mark.parametrize("filepath, should_raise", UNSUPPORTED_FILE_CASES)
+def test_unsupported_raise(filepath: Path, should_raise: bool) -> None:
+    """Test that an error is appropriately raised for unsupported file types."""
+    if should_raise:
+        with pytest.raises(UnsupportedLogFile):
+            iDAQ.parse_log_file(filepath)
+    else:
+        iDAQ.parse_log_file(filepath)
